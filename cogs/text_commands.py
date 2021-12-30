0b556aa60b3ddfce8464724accb781401221eee1
deatg5 <67492418+deatg5@users.noreply.github.com>
2021-12-30 11:46:55 -0700

    Update text_commands.py

--- cogs/text_commands.py
+++ cogs/text_commands.py
@@ -62,7 +62,7 @@ class TextCommands(commands.Cog):
             text = ""
             for i in range(count):
                 emoji = random.choice(self.client.emojis)
-                if emoji.is_usable():
+                if emoji.is_usable() and emoji.available:
                     text += "<:" + str(emoji.name) + ":" + str(emoji.id) + ">"
             async with ctx.typing():
                 await ctx.send(text)
