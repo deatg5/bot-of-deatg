7b6c2151a75124c715109e1bdf022dacfd3f9ff4
deatg5 <67492418+deatg5@users.noreply.github.com>
2022-01-29 10:42:00 -0700

    Update loops.py

--- cogs/loops.py
+++ cogs/loops.py
@@ -56,7 +56,7 @@ class Loops(commands.Cog):
         await self.client.wait_until_ready()
 
 
-    @tasks.loop(seconds=2759)
+    @tasks.loop(seconds=275)
     async def random_dm(self):
         try:
             if random.randint(1, 100) < 30:
