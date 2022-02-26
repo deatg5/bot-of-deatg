df245882723bed858c40d21d91b2f12624e5fa61
deatg5 <67492418+deatg5@users.noreply.github.com>
2022-02-26 01:35:59 -0700

    Update listeners.py

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -55,7 +55,7 @@ class Listeners(commands.Cog):
     async def decide_message(self, message):
         message_to_send = Common.random_message(self)
 
-        message_type = random.randint(0, 280)
+        message_type = random.randint(0, 250)
 
         if 0 <= message_type <= 172:
             message_to_send = Common.random_message(self)
