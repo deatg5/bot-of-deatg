6f1d59c33603bcfe64e4ddad1d0ac50a7dd4f237
deatg5 <67492418+deatg5@users.noreply.github.com>
2023-03-13 22:48:52 -0600

    f
    f

--- cogs/image_commands.py
+++ cogs/image_commands.py
@@ -98,7 +98,7 @@ class ImageCommands(commands.Cog):
             selected_file = random.choice(os.listdir("images/"))
             img = Image.open("images/" + selected_file)
         else:
-            img = Image.open(requests.get(image, stream=True).raw)
+            img = ""
         loop_times = random.randint(1, 16)
         for i in range(loop_times):
             members = ctx.guild.members
@@ -148,7 +148,7 @@ class ImageCommands(commands.Cog):
             selected_file = random.choice(os.listdir("images/"))
             img = Image.open("images/" + selected_file)
         else:
-            img = Image.open(requests.get(image, stream=True).raw)
+            img = ""
         loop_times = random.randint(1, 16)
         await ctx.defer()
         for i in range(loop_times):
