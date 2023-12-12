9d54b168916d7f9e23925672e5fe7a6b52ff7876
deatg5 <67492418+deatg5@users.noreply.github.com>
2023-12-11 18:55:49 -0700

    added microsoft edge tts (courtesy of mr. F53) :D

--- cogs/music_commands.py
+++ cogs/music_commands.py
@@ -5,6 +5,7 @@ from discord.utils import get
 import youtube_dl
 import os
 from gtts import gTTS
+from edge_tts import Communicate
 import asyncio
 
 from cogs.common import Common
@@ -26,159 +27,51 @@ class MusicCommands(commands.Cog):
 
     voice_clients = {}
     
-    @commands.slash_command(name="join", description="join voice channel")
-    async def join(self, ctx):
+    async def try_join(self, ctx):
+        if ctx.guild.id in self.voice_clients:
+            await ctx.respond("I'm already connected to a voice channel in this server.")
+            return False
+
+        if ctx.author.voice is None:
+           await ctx.respond("You must be in a voice channel to use this command.")
+           return False
+
         voice_client = await ctx.author.voice.channel.connect()
         self.voice_clients[voice_client.guild.id] = voice_client
-        await ctx.respond(f"joined {ctx.author.voice.channel.name}!!")
+        return True
 
-    @commands.slash_command(name="deafen", description="deafen the bot in a voice channel")
-    async def deafen(self, ctx):
-        await self.client.get_member(self.client.id).edit(deafen=True)
-
+    @commands.slash_command(name="join", description="join voice channel")
+    async def join(self, ctx):
+        if (await self.try_join(ctx)):
+            await ctx.respond(f"joined {ctx.author.voice.channel.name}")
 
     @commands.slash_command(name="tts", description="Generate TTS audio from text and play it in a voice channel.")
     async def tts(self, ctx, text: str):
-        #if ctx.author.voice is None:
-        #    await ctx.respond("You must be in a voice channel to use this command.")
-        #    return
-        
-        voice_client = None
+        # get current voice channel / join one that the user is in
+        if ctx.guild.id not in self.voice_clients and not await self.try_join(ctx):
+            return
+        voice_client = self.voice_clients[ctx.guild.id]
 
-        if not self.voice_clients[ctx.guild.id]:
-            voice_client = await ctx.author.voice.channel.connect()
-            self.voice_clients[voice_client.guild.id] = voice_client
-        else:
-            voice_client = self.voice_clients[ctx.guild.id]
+        # Generate audio from text
+        await Communicate(text, 'en-IE-EmilyNeural').save('tts.mp3')
 
-        tts = gTTS(text=text, lang='en')
-        tts.save("tts.mp3")
+        # Stop playback if already playing
+        if voice_client.is_playing():
+            voice_client.stop()
 
+        # Start speaking the TRUTH
         voice_client.play(discord.FFmpegPCMAudio("tts.mp3"))
 
-        while voice_client.is_playing():
-            await asyncio.sleep(1)
-        await ctx.respond(f"I SPEAK{'!' * random.randint(0, 100)} {random.choice(Lists.all_emoji)}")
+        # respond to command
+        await ctx.respond(f"I say: \"{text}\"")
 
-            
     @commands.slash_command(name="leave", description="bot leave voice channel")
     async def leave(self, ctx):
-        await ctx.author.voice.channel.disconnect()
+        await ctx.guild.voice_client.disconnect()
         self.voice_clients[ctx.guild.id] = None
-        
-
-    #@commands.command(pass_context=True, brief="stop the currently playing music")
-    #async def rejoin(self, ctx):
-    #    channel = ctx.message.author.voice.channel
-    #    voice = get(self.client.voice_clients, guild=ctx.guild)
-    #    if voice and voice.is_connected():
-    #        await voice.disconnect()
-    #    channel = ctx.message.author.voice.channel
-    #    voice = get(self.client.voice_clients, guild=ctx.guild)
-    #    if voice and voice.is_connected():
-    #        await voice.move_to(channel)
-    #    else:
-    #        voice = await channel.connect()
-    #    await voice.disconnect()
-    #    if voice and voice.is_connected():
-    #        await voice.move_to(channel)
-    #    else:
-    #        voice = await channel.connect()
+        await ctx.respond("ok bye")
 
 
-
-    #@commands.command(pass_context=True, brief=";play [YouTube url or search term]")
-    #async def play(self, ctx, *url: str):
-    #    if "index" in url:
-    #        await ctx.send("Sorry, but you can't use a playlist link.")
-    #        return
-    #    song_there = os.path.isfile("song.mp3")
-    #    try:
-    #        if song_there:
-    #            os.remove("song.mp3")
-    #    except PermissionError:
-    #        await ctx.send("Wait for the current playing music to end or use the 'leave' command")
-    #        return
-    #    await ctx.send("Getting audio from YouTube... this may take a long time (and maybe even crash the entire bot)")
-    #    await Common.log(self, f'played {str(url)}', ctx)
-    #    print("Someone wants to play music let me get that ready for them...")
-    #    voice = get(self.client.voice_clients, guild=ctx.guild)
-    #    ydl_opts = {
-    #        'format': 'bestaudio/best',
-    #        'postprocessors': [{
-    #            'key': 'FFmpegExtractAudio',
-    #            'preferredcodec': 'mp3',
-    #            'preferredquality': '192',
-    #        }],
-    #    }
-    #    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #        try:
-    #            ydl.download([url])
-    #        except:
-    #            search_term = " ".join(url[:])
-    #            ydl.extract_info(f"ytsearch:{search_term}", download=True)['entries'][0]
-    #    for file in os.listdir("./"):
-    #        if file.endswith(".mp3"):
-    #            os.rename(file, 'song.mp3')
-    #    try:
-    #        voice.play(discord.FFmpegPCMAudio("song.mp3"))
-    #    except:
-    #        channel = ctx.message.author.voice.channel
-    #        voice = get(self.client.voice_clients, guild=ctx.guild)
-    #        if voice and voice.is_connected():
-    #            await voice.disconnect()
-    #        channel = ctx.message.author.voice.channel
-    #        voice = get(self.client.voice_clients, guild=ctx.guild)
-    #        if voice and voice.is_connected():
-    #            await voice.move_to(channel)
-    #        else:
-    #            voice = await channel.connect()
-    #        await voice.disconnect()
-    #        if voice and voice.is_connected():
-    #            await voice.move_to(channel)
-    #        else:
-    #            voice = await channel.connect()
-    #        voice.play(discord.FFmpegPCMAudio("song.mp3"))
-    #    voice.volume = 100
-    #    voice.is_playing()
-
-    #@commands.slash_command(name="play", description="play tts")
-    #async def play(self, ctx):
-    #    channel = ctx.author.voice.channel
-    #    vc = await channel.connect()
-    #    vc.play(discord.FFmpegPCMAudio('C:\\Users\\Nick\\Documents\\bot_of_deatg\\PYCORD\\tts.mp3'))
-
-    
-    #@commands.command(pass_context=True, brief="plays a totally random song from youtube")
-    #async def playrandom(self, ctx):
-    #    url = random.choice(Lists.youtube_song_list)
-    #    song_there = os.path.isfile("song.mp3")
-    #    try:
-    #        if song_there:
-    #            os.remove("song.mp3")
-    #    except PermissionError:
-    #        await ctx.send("Wait for the current playing music to end or use the 'leave' command")
-    #        return
-    #    await ctx.send("Getting audio from YouTube... this may take a long time (and maybe even crash the entire bot)")
-    #    Common.log(self, f'played {str(url)} from Updated', ctx)
-    #    print("Someone wants to play music let me get that ready for them...")
-    #    voice = get(self.client.voice_clients, guild=ctx.guild)
-    #    ydl_opts = {
-    #        'format': 'bestaudio/best',
-    #        'postprocessors': [{
-    #            'key': 'FFmpegExtractAudio',
-    #            'preferredcodec': 'mp3',
-    #            'preferredquality': '192',
-    #        }],
-    #    }
-    #    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #        ydl.download([url])
-    #    for file in os.listdir("./"):
-    #        if file.endswith(".mp3"):
-    #            os.rename(file, 'song.mp3')
-    #    voice.play(discord.FFmpegPCMAudio("song.mp3"))
-    #    voice.volume = 100
-    #    voice.is_playing()
 
 def setup(client):
     client.add_cog(MusicCommands(client))
