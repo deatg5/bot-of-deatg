aa2f0a02570fae06dbeff1cb0b9158c5099da976
deatg5 <67492418+deatg5@users.noreply.github.com>
2024-07-21 02:13:47 -0600

    Update music_commands.py

--- cogs/music_commands.py
+++ cogs/music_commands.py
@@ -13,31 +13,44 @@ class MusicCommands(commands.Cog):
         self.client = client
         self.voice_clients = {}
 
+    async def ensure_voice_state(self, ctx):
+        """Ensure the bot's voice state is consistent with reality."""
+        if ctx.guild.id in self.voice_clients:
+            voice_client = self.voice_clients[ctx.guild.id]
+            if not voice_client.is_connected():
+                del self.voice_clients[ctx.guild.id]
+            elif voice_client.channel != ctx.author.voice.channel:
+                await voice_client.move_to(ctx.author.voice.channel)
+
     @commands.slash_command(name="join", description="join voice chanel :dove:")
     async def join(self, ctx):
         if ctx.author.voice is None:
-            await ctx.send("you is not in a voice channel!!!")
+            await ctx.respond("you is not in a voice channel!!!")
             return
+        await self.ensure_voice_state(ctx)
+
         voice_channel = ctx.author.voice.channel
         if ctx.guild.id in self.voice_clients:
             await self.voice_clients[ctx.guild.id].move_to(voice_channel)
         else:
             self.voice_clients[ctx.guild.id] = await voice_channel.connect()
-        await ctx.reply(f"joined {voice_channel.name} :3")
+        await ctx.respond(f"joined {voice_channel.name} :3")
 
     @commands.slash_command(name="leave", description="get OUT of the voice channel")
     async def leave(self, ctx):
+        await self.ensure_voice_state(ctx)
         if ctx.guild.id in self.voice_clients:
             await self.voice_clients[ctx.guild.id].disconnect()
             del self.voice_clients[ctx.guild.id]
-            await ctx.reply("bye bye..")
+            await ctx.respond("bye bye..")
         else:
-            await ctx.reply("ERROUR: i can't leave something i'm not in !!!!")
+            await ctx.respond("ERROUR: i can't leave something i'm not in !!!!")
 
     @commands.slash_command(name="tts", description="make bot of deatg speak!")
     async def tts(self, ctx, *, text):
+        await self.ensure_voice_state(ctx)
         if ctx.guild.id not in self.voice_clients:
-            await ctx.reply("this command is for playing tts audio in voice channels :3")
+            await ctx.respond("this command is for playing tts audio in voice channels :3")
             return
         tts = gtts.gTTS(text)
         fp = io.BytesIO()
@@ -47,10 +60,14 @@ class MusicCommands(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        if member == self.client.user and after.channel is None:
-            guild_id = before.channel.guild.id
-            if guild_id in self.voice_clients:
-                del self.voice_clients[guild_id]
+        if member == self.client.user:
+            if after.channel is None:
+                guild_id = before.channel.guild.id
+                if guild_id in self.voice_clients:
+                    del self.voice_clients[guild_id]
+            elif before.channel != after.channel:
+                guild_id = after.channel.guild.id
+                self.voice_clients[guild_id] = member.guild.voice_client
 
 
 
