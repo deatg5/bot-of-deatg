3968dae821cc41a04c05823f57f6b29b37362fa6
deatg5 <villager23976@gmail.com>
2025-11-10 21:30:43 -0700

    Update lists.py

--- cogs/lists.py
+++ cogs/lists.py
@@ -572,7 +572,8 @@ class Lists(commands.Cog):
                 "JOLLIZOOM PRAYERS\nMicrosoft Teams Meeting\nTomorrow all day","people getting mad at me for hating gnomes? open a terminal and /usr/sbin/remove-gnome come on  now bruh","让我们成为朋友 (´• ω •`)","I am a bot of deatg, I am not a bot of deatg, I am a bot of deatg, I am not a bot of deatg, I am a bot of deatg, I am not a bot of deatg, I am a bot of deatg, I am not a bot of deatg, I am a bot of deatg, I am not a bot of deatg, I am a bot of deatg, I am not a bot of deatg, I am a bot of deatg, I am not a bot of deatg, I am a bot of deatg, I am not a bot of deatg",
                 "+rep good places for plant","+rep AWP boss","hi bro! add me please :)","add me for future games!","nah u good beautiful powerful woman","i didn't even know his name until he signed into my chromebook",
                 "9055508581","fuck offffffff.... btfo.... hoe","\*grumbe\*(The goo","CUTIE ALERT! CUTIE ALERT!","飲むと頭がｂｏｔ ｏｆ ｄｅａｔｇ","You Will Be Receiving Exciting Special And Seasonal Promations! Thanks!","i am kind of surfing inside",
-                "what happened to your account?","🦌 dogos dinted"]
+                "what happened to your account?","🦌 dogos dinted","おいでよ！柴又","from left to right it’s big justice, lil cumtism, and the rizzler","Let's play game I got 3 energy drink and my inhaler and gum I'm ready to seduce middle aged women and shoot British people",
+                "Life so private no one knows I caught a hat besides me and buddha🪖"]
     
 
     messages_chrstimas = [
